37e6d7bb1b72e697b1424f9d8446401903000ec9
Valerie Lui <vallq@users.noreply.github.com>
2020-05-19 18:09:17 +0800

    add POST method for todolist

--- todo-backend/routes/todoList.route.js
+++ todo-backend/routes/todoList.route.js
@@ -2,13 +2,32 @@ const express = require("express");
 const router = express.Router();
 const TodoItem = require("../models/todoItem.model");
 
-router.get("/", async (req, res) => {
+const getAllTodoItems = async (req, res) => {
   const includesAll = new RegExp(/./);
   const todoList = await TodoItem.find(
     { id: includesAll },
     "id value completed"
   );
   res.status(200).send(todoList);
+}
+
+const postNewTodoItem = async (req, res) => {
+  try {
+    const itemToPost = new TodoItem(req.body);
+    await itemToPost.save();
+    res.status(201).send(itemToPost);
+  } catch (err) {
+    err.message = new Error("unable to post item");
+    err.statusCode = 400;
+    next(err);
+  }
+}
+
+router.get("/",getAllTodoItems);
+router.post("/", postNewTodoItem);
+
+router.use((err, req, res, next) => {
+  next(err);
 });
 
 module.exports = router;
