642ac7131a3238b436c9da2b4b8f95d18bb8c74c
Valerie Lui <vallq@users.noreply.github.com>
2020-05-20 21:52:45 +0800

    add cors to allow api calls from different origin

--- todo-backend/app.js
+++ todo-backend/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const app = express();
 const apiEndpoints = require("./apiEndpoints");
 const todoListRouter = require("./routes/todoList.route");
+const cors = require('cors');
+const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/todolist", todoListRouter);
 
