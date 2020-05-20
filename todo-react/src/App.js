e2f640507be3ee3dad60da6473f4107e1f90db8c
Valerie Lui <vallq@users.noreply.github.com>
2020-05-20 21:54:25 +0800

    add POST call to API

--- todo-react/src/App.js
+++ todo-react/src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
 import logo from "./todo.svg";
 import "./App.css";
+//import TodoItem from "./components/TodoItem";
+import axios from "axios";
+import { v4 as uuidv4 } from "uuid";
 
+const API = "http://localhost:3001/todolist";
 const ENTER_KEY = 13;
 const ALL_TODOS = "all";
 //const ACITVE_TODOS = "active";
@@ -14,12 +18,34 @@ class App extends React.Component {
       newTodo: "",
       editing: null,
       todoItems: [],
-      nowShowing: ALL_TODOS 
+      nowShowing: ALL_TODOS,
     };
   }
 
   handleChange = (event) => {
     this.setState({ newTodo: event.target.value });
+  };
+
+  createTodoObject = () => {
+    const todoObject = {
+      id: uuidv4(),
+      value: this.state.newTodo,
+      completed: false,
+    };
+    return todoObject;
+  };
+
+  addTodo = async () => {
+    const todoToAdd = this.createTodoObject();
+    console.log(todoToAdd);
+    const response = await axios.post(API, todoToAdd);
+    const currentTodoItems = this.state.todoItems;
+    currentTodoItems.push(response);
+    this.updateTodoList(currentTodoItems);
+  };
+
+  updateTodoList = (updatedList) => {
+    this.setState({ todoItems: updatedList });
   };
 
   handleNewTodoKeyDown = (event) => {
@@ -32,42 +58,94 @@ class App extends React.Component {
     var val = this.state.newTodo.trim();
 
     if (val) {
-      //this.props.addTodo(val);
-      this.setState({newTodo: ''});
+      this.addTodo(val);
+      this.setState({ newTodo: "" });
     }
-  }
+  };
 
   toggleAll = (event) => {
     //var checked = event.target.checked;
     //this.props.toggleAll(checked);
-  }
+  };
 
   toggle = (todo) => {
     //this.props.toggle(todo);
-  }
+  };
 
   destroy = (todo) => {
     //this.props.destroy(todo);
-  }
+  };
 
   edit = (todo) => {
     this.setState({ editing: todo.id });
-  }
+  };
 
   save = (todo, text) => {
-    this.props.save(todo, text);
+    //this.props.save(todo, text);
     this.setState({ editing: null });
-  }
+  };
 
   cancel = (event) => {
-    this.setState({editing: null});
-  }
+    this.setState({ editing: null });
+  };
 
   clearCompleted = () => {
     //this.props.clearCompleted;
-  }
+  };
 
   render() {
+    let footer;
+    let main;
+    //let todos = this.state.todoItems;
+
+    // let shownTodos = todos.filter((todo) => {
+    //   switch (this.state.nowShowing) {
+    //     case ACTIVE_TODOS:
+    //       return !todo.completed;
+    //     case COMPLETED_TODOS:
+    //       return todo.completed;
+    //     default:
+    //       return true;
+    //   }
+    // });
+
+    // let todoItems = shownTodos.map((todo) => {
+    //   return (
+    //     <TodoItem
+    //       key={todo.id}
+    //       todo={todo}
+    //       onToggle={this.toggle.bind(this, todo)}
+    //       onDestroy={this.destroy.bind(this, todo)}
+    //       onEdit={this.edit.bind(this, todo)}
+    //       editing={this.state.editing === todo.id}
+    //       onSave={this.save.bind(this, todo)}
+    //       onCancel={this.cancel}
+    //     />
+    //   );
+    // });
+
+    // let activeTodoCount = todos.reduce((accum, todo) => {
+    //   return todo.completed ? accum : accum + 1;
+    // }, 0);
+
+    //let completedCount = todos.length - activeTodoCount;
+
+    // if (todos.length) {
+    //   main = (
+    //     <section className="main">
+    //       <input
+    //         id="toggle-all"
+    //         className="toggle-all"
+    //         type="checkbox"
+    //         onChange={this.toggleAll}
+    //         checked={activeTodoCount === 0}
+    //       />
+    //       <label htmlFor="toggle-all" />
+    //       <ul className="todo-list">{todoItems}</ul>
+    //     </section>
+    //   );
+    // }
+
     return (
       <div className="App">
         <div>
@@ -97,8 +175,8 @@ class App extends React.Component {
               autoFocus={true}
             />
           </header>
-          {/* {main}
-            {footer} */}
+          {main}
+          {footer}
         </div>
       </div>
     );
