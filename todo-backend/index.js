e42816e4f13e3a9708212d05b4430db2c9e0dc19
Valerie Lui <vallq@users.noreply.github.com>
2020-05-20 10:13:47 +0800

    add db access to index.js

--- todo-backend/index.js
+++ todo-backend/index.js
@@ -1,5 +1,6 @@
 const PORT = 3001;
 const app = require("./app");
+require("./utils/db");
 
 const server = app.listen(PORT, () => {
   console.log(`todo express started on http://localhost:${PORT}`);
